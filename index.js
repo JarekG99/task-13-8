52c53d2925d75dbf64ef6ab3fdb676c1063c4988
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-03-02 16:56:31 +0100

    Clear bugs

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var fs = require('fs');
 var http = require('http');
 var colors = require('colors');
 
+function getData(err, data) {
+     console.log('Data to be sent:'.blue, data);
+    this.write(data);
+    this.end();
+}
 
-fs.readFile('./index.html','utf-8', function(err, data) {
-  console.log('Data to be sent:'.blue, data);
-
-        });
   var server = http.createServer();
 
   server.on('request', function (request, response) {
       response.setHeader("Content-Type", "text/html; charset=utf-8");
-      if (request.method === 'GET' && request.url === '/.') {
-            response.write(data);
-                    response.end();
+      if (request.method === 'GET' && request.url === '/') {
+          fs.readFile('./index.html','utf-8', getData.bind(response));
             } else {
                     response.statusCode = 404;
                     response.write('<h1>404: The page does not exist!</h1>');
